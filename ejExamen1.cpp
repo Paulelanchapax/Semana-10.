44512001d441901308979d0f65a7ecbf704e3496
Paulelanchapax <2005paulalejandro@gmail.com>
2026-04-13 23:22:47 -0500

    Subiendo ejercicio corregido de prueba (1)

---
+++ ejExamen1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+
+cout<<"Ingrese el primer número:";
+cin>> a;
+
+cout<<"Ingrese el segundo número:";
+cin>> b;
+
+cout<<"Suma:"<<a+b<<endl;
+cout<<"Resta:"<<a-b<<endl;
+cout<<"Multiplicación"<<a*b<<endl;
+
+return 0;
+}
